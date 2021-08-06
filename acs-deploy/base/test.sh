58027e1b8ef4e66fd9e33f09aa3b626f96042162
rcarrata <rcarrata@redhat.com>
2021-08-06 10:48:43 +0200

    testing gitops

--- acs-deploy/base/test.sh
+++ acs-deploy/base/test.sh
@@ -8,4 +8,6 @@ https://$CENTRAL_ROUTE/v1/cluster-init/init-bundles \
 EOF
 )
 
+TEST=$(curl -X POST -k -H 'Authorization: Basic YWRtaW46c3RhY2tyb3g=' https://$CENTRAL_ROUTE/v1/cluster-init/init-bundles --data '{"name": "production2"}')
+
 oc get svc central -n stackrox -o jsonpath='{.spec.clusterIP}'
